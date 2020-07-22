693aa1d8def2c5bf487ba829595ae2c3af60e1ba
ValJeansEvilBread <64847510+ValJeansEvilBread@users.noreply.github.com>
2020-07-22 16:40:18 -0400

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+ /* BrowserAnimationsModule cannot be found */
+/* import { BrowserAnimationsModule} from '@angular/platform-browser-animations'; */
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
